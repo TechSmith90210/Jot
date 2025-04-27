b1f33027e3f612c0a07a0caa036f1bd932f10150
TechSmith90210 <sshaikhdev@gmail.com>
2025-04-27 16:56:34 +0530

    feat: search screen UI layout complete

--- app/src/main/java/com/mindpalace/app/presentation/screens/home/homeScreen.kt
+++ app/src/main/java/com/mindpalace/app/presentation/screens/home/homeScreen.kt
@@ -1,30 +1,18 @@
 package com.mindpalace.app.presentation.screens.home
 
-import android.R.attr.thickness
-import android.graphics.ColorFilter
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.Indication
-import androidx.compose.foundation.LocalIndication
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.ScrollableDefaults
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -38,20 +26,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.mindpalace.app.R
 import com.mindpalace.app.presentation.components.PageListItem
 import com.mindpalace.app.presentation.components.RecentNotesCard
 import com.mindpalace.app.presentation.components.titles
-import com.mindpalace.app.presentation.theme.Border
-import kotlin.math.log
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable

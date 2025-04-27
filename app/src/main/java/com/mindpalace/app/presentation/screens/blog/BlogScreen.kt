fa6097fdb73d5c0a1b3bfcc361dcd6755c053add
TechSmith90210 <sshaikhdev@gmail.com>
2025-04-27 20:08:08 +0530

    feat: blogs browser screen UI layout complete 1

--- app/src/main/java/com/mindpalace/app/presentation/screens/blog/BlogScreen.kt
+++ app/src/main/java/com/mindpalace/app/presentation/screens/blog/BlogScreen.kt
@@ -14,21 +14,52 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mindpalace.app.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BlogScreen(modifier: Modifier) {
     val pagerState = rememberPagerState(
         pageCount = { pageContents.size })
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Blogs", style = MaterialTheme.typography.titleSmall) },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.sticky_note_add_line),
+                            tint = MaterialTheme.colorScheme.tertiary,
+                            contentDescription = "Search"
+                        )
+                    }
+                },
+                expandedHeight = 40.dp,
+                colors = TopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                    actionIconContentColor = MaterialTheme.colorScheme.onBackground,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onBackground,
+                    scrolledContainerColor = MaterialTheme.colorScheme.background
+                )
+            )
+        },
+
         content = { padding ->
             Box(
                 modifier = modifier.padding(padding),
